fc07f0edd059b69fabc515684b163307003a93d9
Louwrens Labuschagne <louwjlabuschagne@gmail.com>
2017-12-02 09:51:49 +0200

    adding gists... Forgot where they live, and makes sense moving them to this repo

--- ltxCreator.py
+++ ltxCreator.py
@@ -1,9 +1,7 @@
 import os
-pwd = os.path.abspath(os.path.curdir)
+import requests
 
 def returnGist(link):
-
-	import requests
 	# link = "https://gist.githubusercontent.com/louwjlabuschagne/c360ddcf0a098e0dd2b7d51e37fda204/raw/f6bd209edc3a08e2cefbe4634d8b1b1d96f1010c/Gist.tex"
 	f = requests.get(link)
 	return(f.text)
@@ -28,10 +26,12 @@ def touch(_file, _dir, gist_url):
 		print("retrieving"+_file+" from gist @"+gist_url+" and writing to "+_dir+"/"+_file)
 		f.write(returnGist(gist_url))
 		print("success")
-
-
+#create directories
+pwd = os.path.abspath(os.path.curdir)
 assets = mkdir(pwd, "assets")
 content = mkdir(pwd, "content")
-touch("preAmble.tex", content, "https://gist.githubusercontent.com/louwjlabuschagne/c360ddcf0a098e0dd2b7d51e37fda204/raw/f6bd209edc3a08e2cefbe4634d8b1b1d96f1010c/preAmble.tex")
-touch("main.tex", pwd, "https://gist.githubusercontent.com/louwjlabuschagne/36f18b685e5908d2098b364f69ce637e/raw/54e7673a4b6687de21c470161de476d9ffa1648d/main.tex")
+figures = mkdir(pwd, "figures")
+
+touch("preAmble.tex", _dir=content, "https://gist.githubusercontent.com/louwjlabuschagne/c360ddcf0a098e0dd2b7d51e37fda204/raw/f6bd209edc3a08e2cefbe4634d8b1b1d96f1010c/preAmble.tex")
+touch("main.tex", _dirpwd, "https://gist.githubusercontent.com/louwjlabuschagne/36f18b685e5908d2098b364f69ce637e/raw/54e7673a4b6687de21c470161de476d9ffa1648d/main.tex")
 
